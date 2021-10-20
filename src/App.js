380ca43159b179f397b7b4694785a79ec9e1ecb0
Douglas Falcao <falcao.douglas97@gmail.com>
2021-10-20 00:49:25 -0300

    v2.0

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import { useState } from 'react';
 
 function App() {
     const [query, setQuery] = useState("");
-    // const [searchResult, setSearchResult] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
     // const [userName, setUserName] = useState("");
     // const [isLoading, setIsLoading] = useState(false);
 
     const handleSearch = async() => {
       await fetch(`https://api.github.com/search/users?q=${query}`)
       .then((response) => response.json())
-      .then((data) => {console.log(data)});
+      .then((data) => {setSearchResults(data.items)});
     }
   return (
     <main>
 
    <section className="search">
-     <input type="search" value={query} onChange={({target}) => setQuery(target.value)} placeholder="Username" readOnly={true}/>
+     <input type="search"
+      value={query}
+       onChange={({ target }) => setQuery(target.value)}
+        placeholder="Username"/>
      <button onClick={handleSearch}>Pesquisar</button>
+
+     <h1>Resultados da Busca</h1>
+
+     <ul>
+       {searchResults.map((user) => (
+         <li key={user.id}>
+           <img src={user.avatar_url} alt={`Foto de ${user.login}`}/>
+         </li>
+       ))}
+     </ul>
    </section>
 
    <section className="repos">Repositório</section>
