b802088a6be5dfb6cf24c8f78d16af6f0f750157
Douglas Falcao <falcao.douglas97@gmail.com>
2021-10-21 01:18:02 -0300

    3.0

--- src/App.js
+++ src/App.js
@@ -2,39 +2,62 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-    const [query, setQuery] = useState("");
-    const [searchResults, setSearchResults] = useState([]);
-    // const [userName, setUserName] = useState("");
-    // const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleSearch = async() => {
-      await fetch(`https://api.github.com/search/users?q=${query}`)
+  const handleSearch = async () => {
+
+  if(query === ""){
+    setQuery("a");
+  }
+    setIsLoading(true);
+    await fetch(`https://api.github.com/search/users?q=${query}`)
       .then((response) => response.json())
-      .then((data) => {setSearchResults(data.items)});
-    }
+      .then((data) => { setSearchResults(data.items) });
+
+    setIsLoading(false)
+  }
+
   return (
     <main>
 
-   <section className="search">
-     <input type="search"
-      value={query}
-       onChange={({ target }) => setQuery(target.value)}
-        placeholder="Username"/>
-     <button onClick={handleSearch}>Pesquisar</button>
+      <section className="search">
+        <input type="search"
+          value={query}
+          onChange={({ target }) => setQuery(target.value)}
+          placeholder="Username" />
 
-     <h1>Resultados da Busca</h1>
+        <button onClick={handleSearch}>
+          {isLoading ? "Pesquisando..." : "Pesquisar"}
+        </button>
 
-     <ul>
-       {searchResults.map((user) => (
-         <li key={user.id}>
-           <img src={user.avatar_url} alt={`Foto de ${user.login}`}/>
-         </li>
-       ))}
-     </ul>
-   </section>
+        {!!searchResults.length && (
+        <>
+        <h1>Resultados da Busca</h1>
 
-   <section className="repos">Repositório</section>
-   </main>
+        <ul>
+          {searchResults.map((user) => (
+            <li key={user.id}>
+              <img src={user.avatar_url} alt={`Foto de ${user.login}`} />
+
+              {user.login}
+
+              {userName === user.login ? (
+                "✅") : (
+                <button onClick={() => setUserName(user.login)}>Selecionar</button>
+              )}
+            </li>
+          ))}
+        </ul>
+        </>
+        )}
+       
+      </section>
+
+      <section className="repos">Repositório</section>
+    </main>
   );
 }
 
